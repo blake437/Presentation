beb03e3b7542624a810c0a334c6d22744fb65714
Qwerty <qwertyuiop@webname.com>
2025-05-18 14:20:57 -0700

    Update Script.js
    Signed-off-by: Qwerty <qwertyuiop@webname.com>

--- Script.js
+++ Script.js
@@ -1,4 +1,6 @@
 var str = window.location.href.split('?')[1]
 if (JSON != null){
   var obj = json.parse(str)
+  document.getElementById("title").text = obj.content.title
+  document.getElementById("subtitle").text = obj.content.subtitle
 }
